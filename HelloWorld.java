9066dd6f0548007826e27682c53fa1c8e4cbb17c
oleksandrhrynok <112888701+oleksandrhrynok@users.noreply.github.com>
2022-09-11 11:44:48 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        // integer types: int, bytes, short, long
-        //int a;
-        a = 10;
-        System.out.println(a);
+        System.out.println("Hello, world!");
     }
 }
