fa0b345d700cb44c0344034071b96dba9dd87ab4
oleksandrhrynok <112888701+oleksandrhrynok@users.noreply.github.com>
2022-10-19 15:54:17 +0200

    Add files via upload

---
+++ HomeWorkOne.java
@@ -0,0 +1,33 @@
+public class HomeWorkOne {
+    public static void main(String[] args) {
+
+        // home work 1.1
+
+        char c = 'G';
+        int i = 89;
+        byte bt = 4;
+        short sh = 56;
+        float f = 4.733436f;
+        long l = 12123;
+        boolean bl = true;
+
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(bt);
+        System.out.println(sh);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(bl);
+
+        //home work 1.2
+
+        int n = 365;
+        int e = n % 10;
+        int d = (n % 100) / 10;
+        int s = n / 100;
+
+        System.out.println(e);
+        System.out.println(d);
+        System.out.println(s);
+    }
+}
