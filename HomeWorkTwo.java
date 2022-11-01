5da8a2da56d82740ffed05d5fd7a1fec727889da
oleksandrhrynok <112888701+oleksandrhrynok@users.noreply.github.com>
2022-11-01 16:21:08 +0200

    Add files via upload

---
+++ HomeWorkTwo.java
@@ -0,0 +1,68 @@
+public class HomeWorkTwo {
+        public static void main(String[] args) {
+
+            //Home work 1.1
+
+            int x = 54;
+            int y = 16;
+
+            //Home work 1.2
+
+            int a = 50;
+            int b = 3;
+
+            //Home work 1.3
+
+            int c = -5;
+            int d = 8;
+            int f = 6;
+            int q = 55;
+            int z = 9;
+            int z1 = 9;
+
+            //Home work 1.4
+
+            int n = 4;
+
+            //Home work 1.5
+
+            int p = 0;
+            int p1 = 1;
+            int p2 = 2;
+            int p3 = 3;
+            int p4 = 4;
+            int p5 = 5;
+            int p6 = 6;
+            int p7 = 7;
+            int p8 = 8;
+            int p9 = 9;
+            int sum = p + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
+            int p10 = sum / 10;
+            double p11 = sum;
+            double p12 = p11 / 10;
+            double p13 = p12 - p10;
+
+            //Home work 1.6
+
+            int A = 1000;
+            int B = 500;
+            int discount = 100;
+            int Sum = (A + B) - discount;
+
+
+
+            System.out.println(x + y);
+            System.out.println(a / b);
+            System.out.println(c + d * f);
+            System.out.println((q + z) % z1);
+            System.out.println(n * n);
+            System.out.println(p10);
+            System.out.println("Програма отбросила в дробной части = " + p13);
+            System.out.println("Сума со скидкой = " + Sum);
+            System.out.println("Размер скидки = " + discount);
+
+
+        }
+}
+
+
